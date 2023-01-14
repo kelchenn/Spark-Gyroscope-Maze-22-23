fc8a2f345f6cbb4b95cf28423292103e40f5f25a
LEE ZHENG <61095332+leeminze214@users.noreply.github.com>
2023-01-14 15:29:28 -0500

    Update LEDStrip.ino
    created waterfall rainbow color animation for user wins

--- LEDStrip.ino
+++ LEDStrip.ino
@@ -4,9 +4,18 @@
 #define LED_TYPE NEOPIXEL
 //simulating win
 bool win = true;
- 
 //creating array for led manipulation
 CRGB leds[NUM_LEDS];
+/*
+int purp[3] = {129, 2, 129};
+int blue[3] = {0, 0, 255};
+int green[3] = {123, 245, 2};
+int yellow[3] = {248, 248, 7};
+int orange[3] = {253, 166, 6};
+int red[3] = {255,0,0};
+*/
+int colors[6][3] = {{129, 2, 129},{0, 0, 255},{123, 245, 2},{248, 248, 7},{253, 166, 6},{255,0,0}};
+int moveColorCount = 0;
 
 void setup() {
   // setup an array (led) so we can manipulate it
@@ -18,13 +27,20 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (win){
     //if user wins, a set of celebration lighting pattern
+   
+   //rbg rainbow animation
     for (int i=0;i<NUM_LEDS;i++){
-      leds[i] = CRGB::LawnGreen;
-    }
-      FastLED.show();
-      delay(300);
+     int color = (i+moveColorCount)%6;
+     leds[i] = CRGB(colors[color][0],colors[color][1],colors[color][2]);
+   }
+    moveColorCount++;
+    FastLED.show();
+    delay(100);
+   
   }else{
     //if user loses, a diff set of lighting pattern
+   
+   //flashing red and black animation
     for (int i=0;i<NUM_LEDS;i++){
       leds[i] = CRGB::Red;
     }
