55a3c3d9d1c904bb3c7d09c6e0a08327f6d55215
Kelly <58638328+kelchenn@users.noreply.github.com>
2023-02-04 14:40:13 -0500

    add multiple backpacks to be controlled separately

--- timer_backpack.ino
+++ timer_backpack.ino
@@ -2,26 +2,36 @@
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
 
-Adafruit_7segment matrix = Adafruit_7segment();
+Adafruit_7segment matrix0 = Adafruit_7segment();
+Adafruit_7segment matrix1 = Adafruit_7segment();
 
 void setup() {
 #ifndef __AVR_ATtiny85__
   Serial.begin(9600);
   Serial.println("7 Segment Backpack Test");
 #endif
-  matrix.begin(0x77); // for arduino uno
-  // matrix.begin(0x70); // for arduino mega
+  matrix0.begin(0x70);
+  matrix1.begin(0x77);
 }
 
 void loop() {
-  uint16_t blinkcounter = 0;
   for (uint16_t counter = 0; counter < 9999; counter ++) {
     matrix.writeDigitNum(0, (counter / 1000));
     matrix.writeDigitNum(1, (counter / 100) % 10);
     matrix.writeDigitNum(3, (counter / 10) % 10, true);
     matrix.writeDigitNum(4, counter % 10);
 
-    matrix.writeDisplay();
+    matrix0.writeDisplay();
+    delay(100);
+  }
+
+  for (uint16_t counter = 100; counter < 9999; counter ++) {
+    matrix.writeDigitNum(0, (counter / 1000));
+    matrix.writeDigitNum(1, (counter / 100) % 10);
+    matrix.writeDigitNum(3, (counter / 10) % 10, true);
+    matrix.writeDigitNum(4, counter % 10);
+
+    matrix1.writeDisplay();
     delay(100);
   }
 }
