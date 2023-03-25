62879994a6ca81cfcb596fc197a4e1d280784a22
regiszhao <zhaoregis@gmail.com>
2023-03-25 16:16:09 -0400

    added mpu stuff

--- main/main.ino
+++ main/main.ino
@@ -27,6 +27,10 @@
 
 /****START OF GLOBAL VARIABLES****/
 
+// MPU 
+const int MPU = 0x68; // I2C address of the MPU-6050
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+
 //gameplay
 int state; 
 //timing
@@ -213,6 +217,35 @@ void secondLetter();
 /****END OF FUNCTION PROTOTYPES****/
 
 /****START OF FUNCTION IMPLEMENTATIONS****/
+
+void MPU() {
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 14, true); // request a total of 14 registers
+  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  Serial.print("Accelerometer: ");
+  Serial.print("X = "); Serial.print(AcX);
+  Serial.print(" | Y = "); Serial.print(AcY);
+  Serial.print(" | Z = "); Serial.println(AcZ);
+  
+  //equation for temperature in degrees C from datasheet
+  Serial.print("Temperature: "); Serial.print(Tmp / 340.00 + 36.53); Serial.println(" C ");
+
+  Serial.print("Gyroscope: ");
+  Serial.print("X = "); Serial.print(GyX);
+  Serial.print(" | Y = "); Serial.print(GyY);
+  Serial.print(" | Z = "); Serial.println(GyZ);
+  Serial.println(" ");
+  delay(333);
+}
 
 int detectSection() {
   // sensor waiting to detect something
@@ -478,6 +511,14 @@ void setup() {
   pinMode(arcadebutton1Pin, INPUT);
   pinMode(arcadebutton2Pin, INPUT);
   Display(Init_Display);
+
+  //MPU 
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  // Serial.begin(9600);
 }
 
 void loop() {
@@ -496,5 +537,8 @@ void loop() {
         sectionThree();
         break;
     }
+
+  // MPU 
+  MPU();
      
 }
