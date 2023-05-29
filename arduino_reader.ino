019ff752edf2192bfadf0c4cf3d154727af7c2df
Kelly <58638328+kelchenn@users.noreply.github.com>
2023-05-29 16:03:54 -0400

    Update arduino_reader.ino to add another sensor

--- arduino_reader.ino
+++ arduino_reader.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 
 void setup() {
+  Serial.begin(9600);
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // function that executes whenever data is received from writer
   pinMode(LED_BUILTIN,OUTPUT);  // sets onBoard LED as output
@@ -12,9 +13,14 @@ void loop() {
 
 void receiveEvent(int howMany) {
    bool trigger = Wire.read(); // receive a character
+   int irsensorTriggered = Wire.read();
+
    if(trigger){
      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED off by making the voltage LOW
+     Serial.println("sensor: ");
+     Serial.println(irsensorTriggered);
    }
+
    if(!trigger){
      digitalWrite(LED_BUILTIN, LOW);  // turn the LED on (HIGH is the voltage level)
    }
