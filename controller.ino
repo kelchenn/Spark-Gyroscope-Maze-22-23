605e97a339142135cc3d623648bc1edbb3b1a596
Kelly <58638328+kelchenn@users.noreply.github.com>
2023-05-29 18:17:21 -0400

    Update controller.ino with comments and updated DAC numbers

--- controller.ino
+++ controller.ino
@@ -57,10 +57,20 @@ Serial.println(y);
 Serial.print("AngleZ= ");
 Serial.println(z);
 Serial.println("-----------------------------------------");
+ 
+// MPU x/y axis are 0/360 degrees when lying flat, want to only move the maze when the controller x/y axis is between 90-0 and 360-270 degrees
+// Motor has 0/360 degrees at the bottom. 
+// It can rotate from 30 to 330 degrees in the counterclock wise direction (300 degree rotation), where 0 volts is 30 degrees and 5 volts is 330 degrees.
+// We want the maze to have a max rotation of 45 degrees from the horizontal, so between 135 and 225 degrees.
+// So every 300 degrees / 5 volts = 60 degrees per 1 volt to the motor.
+// DAC_MAX / 5 = 819 = 60 degrees = 1 volt
+// 819 * 0.75 = 614 for 45 degrees
+// DAC_MAX / 2 = 2047.5, which is 180 degrees for the motor, or the maze resting position that we want
+ 
 if (y >=0 && y <=90){
   //DAC_MAX/5=819 = 1volt=60deg
-  myDAC.analogWrite(map(y, 90, 0, 1638,2047));
+  myDAC.analogWrite(map(y, 90, 0, 1461, 2047));
 }else if (y >=270 && y <=360){
-myDAC.analogWrite(map(y, 360, 270,  2048,2457));
+myDAC.analogWrite(map(y, 360, 270,  2048, 2662));
 }delay(400);
 }
