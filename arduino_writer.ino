a6b285dbdda5e3ff55f8f472bcb6649a86786ee6
Kelly <58638328+kelchenn@users.noreply.github.com>
2023-05-29 16:04:26 -0400

    Update arduino_writer.ino to add another sensor

--- arduino_writer.ino
+++ arduino_writer.ino
@@ -1,25 +1,42 @@
 #include <Wire.h>
 
+#define IRSENSOR0 A0
+#define IRSENSOR1 A1
+
+int irsensorTriggered;
+
 void setup() {
   Serial.begin(9600);
   while (!Serial);
   Wire.begin(); // join i2c bus (address optional for writer)
-  pinMode(A0, INPUT);
+  pinMode(IRSENSOR0, INPUT);
+  pinMode(IRSENSOR1, INPUT);
 }
 
 void loop() {
-  int detect = analogRead(A0);
-  bool trigger = readSensor(detect);
+  bool trigger = readSensor();
   Wire.beginTransmission(8); // transmit to device #8
   Wire.write(trigger);        // sends the given value
+  Wire.write(irsensorTriggered); 
   Wire.endTransmission();    // stop transmitting
   delay(500);
 }
 
 /* read sensor */
-bool readSensor(int detect) {
+bool readSensor() {
+
+  int detect = analogRead(IRSENSOR0);
+
   if (detect < 200) { // ball detected
     return true;
+    irsensorTriggered = 0;
+  } 
+
+  detect = analogRead(IRSENSOR1);
+
+  if (detect < 200) { // ball detected
+    return true;
+    irsensorTriggered = 1;
   } 
 
   return false;
