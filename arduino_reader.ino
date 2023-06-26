4901b1bf6f7dcab665936c51f479d6bfda9fad05
LEE ZHENG <61095332+leeminze214@users.noreply.github.com>
2023-06-26 11:25:35 -0400

    Update arduino_reader.ino

--- arduino_reader.ino
+++ arduino_reader.ino
@@ -5,16 +5,16 @@ int irSensorPrev;
 void setup() {
   Serial.begin(9600);
   Wire.begin(8);                // join i2c bus with address #8
-  Wire.onReceive(receiveEvent); // function that executes whenever data is received from writer
-  pinMode(LED_BUILTIN,OUTPUT);  // sets onBoard LED as output
   irSensorPrev = -1;
 }
 
 void loop() {
+  Wire.onReceive(receiveEvent); // function that executes whenever data is received from writer
   delay(100);
 }
 
 void receiveEvent(int howMany) {
+  
    int irSensorTriggered = Wire.read();
 
    if(irSensorTriggered != irSensorPrev){
